2d5673bf502d3f251e25b31e93122c455d43c30c
aimon <2504632980@qq.com>
2025-06-11 18:22:26 +0800

    chore：修改rollup.config.js中的配置项，新增打包输出umd.js文件

--- rollup.config.js
+++ rollup.config.js
@@ -63,5 +63,37 @@ export default [
       propertyReadSideEffects: false,
       tryCatchDeoptimization: false
     }
+  },
+  // 新增 UMD 打包配置
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/bundle.umd.js', // 指定 UMD 文件输出路径
+      format: 'umd', // 设置输出格式为 UMD
+      name: 'BabylonViewportGizmo', // 全局变量名，可根据项目调整
+      exports: 'named',
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    external: [/@babylonjs\/.*/],
+    plugins: [
+      typescript({ tsconfig: './tsconfig.json' }),
+      resolve({
+        modulesOnly: true,
+        mainFields: ['module', 'main']
+      }),
+      commonjs(),
+      terser({
+        compress: {
+          passes: 2,
+          drop_console: true
+        }
+      })
+    ],
+    treeshake: {
+      moduleSideEffects: false,
+      propertyReadSideEffects: false,
+      tryCatchDeoptimization: false
+    }
   }
-]; 
+];
